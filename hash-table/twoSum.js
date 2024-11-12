1d8186a232ac810a5df89334e733f7dfea99b5ff
nitrogen27 <nitrogen2772@gmail.com>
2024-11-12 11:22:21 +0300

    Update twoSum.js

--- hash-table/twoSum.js
+++ hash-table/twoSum.js
@@ -1,1 +1,12 @@
-
+export function twoSum(array, target){
+ const map = new Map();
+ for(let i = 0; i < array.length; i++){
+   const index = target - array[i];
+   map.set(index, i);
+ }
+ let j = 0;
+ while(!map.has(array[j])){
+   j++;
+ }
+  return [j,map.get(array[j])]
+}
