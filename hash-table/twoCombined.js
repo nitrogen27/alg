ecb1e515853e41a6ac3af753cbbae884e49eca0c
nitrogen27 <nitrogen2772@gmail.com>
2024-11-13 16:00:03 +0300

    Update twoCombined.js

--- hash-table/twoCombined.js
+++ hash-table/twoCombined.js
@@ -1,1 +1,23 @@
+function findPairsThatSumToTarget(nums, target) {
+    const pairs = [];
+    const seen = new Set();
+
+    for (const num of nums) {
+        const complement = target - num;
+        if (seen.has(complement)) {
+            pairs.push([complement, num]);
+        }
+        seen.add(num);
+    }
+
+    return pairs;
+}
+
+// Пример использования:
+const numbers = [2, 3, 5, 7, 5];
+const targetSum = 10;
+const pairs = findPairsThatSumToTarget(numbers, targetSum);
+
+console.log("Пары чисел, сумма которых равна", targetSum, ":");
+pairs.forEach(pair => console.log(pair));
 
