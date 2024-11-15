d750435d0502bb8bc2ae09b7eac9f975969d9c6b
nitrogen27 <nitrogen2772@gmail.com>
2024-11-15 13:52:00 +0300

    Update fourSum.js

--- hash-table/fourSum.js
+++ hash-table/fourSum.js
@@ -18,6 +18,42 @@
 • Регулировка указателей: Если сумма меньше target, перемещаем указатель left вправо для увеличения суммы. Если больше target, перемещаем указатель right влево для уменьшения суммы.
  */
 function fourSum(nums, target) {
+  const fours = [];
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 3; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      if (j > i+1 && nums[j] === nums[j - 1]) continue;
+
+      let left = j + 1;
+      let right = nums.length - 1;
+
+      while (left< right) {
+        let sum = nums[i] + nums[j] + nums[left] + nums[right];
+        if (sum === target) {
+          fours.push([nums[i],nums[j],nums[left],nums[right]]);
+          while (left < right && nums[left] === nums[left + 1]) {
+            left++;
+          }
+          while (left < right && nums[right] === nums[right - 1]) {
+            right--;
+          }
+          left++;
+          right--;
+        }
+        if (sum < target) {
+          left++;
+        }
+        if (sum > target) {
+          right--;
+        }
+      }
+    }
+  }
+  return fours;
+}
+function fourSum(nums, target) {
     const quadruplets = [];
     if (nums.length < 4) return quadruplets;
 
