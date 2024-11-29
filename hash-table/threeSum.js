06de26ab8945c50aed619d1ea744ecd834bce80d
nitrogen27 <nitrogen2772@gmail.com>
2024-11-29 16:12:55 +0300

    Update threeSum.js

--- hash-table/threeSum.js
+++ hash-table/threeSum.js
@@ -35,6 +35,7 @@ function threeSum(nums) {
         }
       }
     }
+    
   }
   return result;
 }
@@ -61,3 +62,31 @@ function optThreeSum(nums) {
   }
   return result;
 }
+
+export function opt2ThreeSum(nums) {
+  nums.sort((a, b) => a - b);
+  const result = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      let target = nums[i] + nums[left] + nums[right];
+      if (target === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+        left++
+        right--
+      }
+      if (target < 0) {
+        left++;
+      } else {
+        right--;
+      }
+      while(left > i + 1 && nums[left] === nums[left-1]) left++
+      while(right < nums.length - 1 && nums[right] === nums[right+1]) right--
+    }
+  }
+  console.log(result);
+  return result;
+}
+
