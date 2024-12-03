26ce7e430cf6e41518664c9cf691b9ad86838fd0
nitrogen27 <nitrogen2772@gmail.com>
2024-12-03 11:16:02 +0300

    Update anagram.js

--- hash-table/anagram.js
+++ hash-table/anagram.js
@@ -1,1 +1,15 @@
-
+ function anagram(ans) {
+  const hash = new Map();
+  for (let i = 0; i < ans.length; i++) {
+    const an = ans[i];
+    const key = Array.from(an)
+      .sort((a, b) => a.localeCompare(b))
+      .join("");
+    if (hash.has(key) && key !== an) {
+      hash.set(key, [...hash.get(key), an]);
+    } else {
+      hash.set(key, [an]);
+    }
+  }
+  return Array.from(hash.values());
+}
