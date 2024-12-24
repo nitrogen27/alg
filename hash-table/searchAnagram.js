c971a25f165f6670cd7421f8d647dcf633fac45d
nitrogen27 <nitrogen2772@gmail.com>
2024-12-24 12:48:20 +0300

    Update searchAnagram.js

--- hash-table/searchAnagram.js
+++ hash-table/searchAnagram.js
@@ -14,23 +14,29 @@ export function searchAnagram(str, target) {
 
   for (let i = 0; i < target.length; i++) {
     if (hashStr.has(target[i])) {
-      hashStr.get(target[i]).forEach((n) => link.set(n, i))
+      hashStr.get(target[i]).forEach((n) => link.set(n, i));
     }
   }
 
-  const flatPrepare = Array.from(link.keys()).flat().sort((a, b) => a - b);
+  const flatPrepare = Array.from(link.keys())
+    .flat()
+    .sort((a, b) => a - b);
 
-  for(let i = 0; i < flatPrepare.length; i++){
+  for (let i = 0; i < flatPrepare.length; i++) {
     let start = i;
     const part = [flatPrepare[i]];
     const partHash = new Set();
-    partHash.add(link.get(flatPrepare[i]))
-    while(flatPrepare[start] + 1 === flatPrepare[start + 1] && part.length < sizeTarget && !partHash.has(link.get(flatPrepare[start + 1]))){
-      partHash.add(link.get(flatPrepare[start + 1]))
-      part.push(flatPrepare[start+ 1])
-      start++
+    partHash.add(link.get(flatPrepare[i]));
+    while (
+      flatPrepare[start] + 1 === flatPrepare[start + 1] &&
+      part.length < sizeTarget &&
+      !partHash.has(link.get(flatPrepare[start + 1]))
+    ) {
+      partHash.add(link.get(flatPrepare[start + 1]));
+      part.push(flatPrepare[start + 1]);
+      start++;
     }
-    if(part.length === sizeTarget) result.push(part[0]);
+    if (part.length === sizeTarget) result.push(part[0]);
   }
   console.log(result);
   return result;
