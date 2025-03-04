fd753d8aa3214f3a5147546e8aef5780c6dae35e
nitrogen27 <nitrogen2772@gmail.com>
2025-03-04 14:24:55 +0300

    Update theory.js

--- bfs/theory.js
+++ bfs/theory.js
@@ -253,4 +253,39 @@ var numIslands = function(grid) {
 };
 
 
-   
+  var numIslands = function(grid) {
+    let islands = 0;
+    const rows = grid.length;
+    const cols = grid[0].length;
+
+    const bfs = (r, c) => {
+        const q = [];
+        q.push([r, c]);
+        grid[r][c] = "0";
+
+        while (q.length > 0) {
+            const [row, col] = q.shift();
+            const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+            for (const [dr, dc] of directions) {
+                const nr = row + dr;
+                const nc = col + dc;
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] === "1") {
+                    q.push([nr, nc]);
+                    grid[nr][nc] = "0";
+                }
+            }
+        }
+    };
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (grid[r][c] === "1") {
+                islands += 1;
+                bfs(r, c);
+            }
+        }
+    }
+
+    return islands;    
+}; 
