6f0f571cfa3b755b7a5382baf679ee8a1a4db131
nitrogen27 <nitrogen2772@gmail.com>
2025-03-07 13:58:24 +0300

    Update findIslands.js

--- bfs/findIslands.js
+++ bfs/findIslands.js
@@ -186,3 +186,34 @@ export function findIsland(grid) {
   console.log(`Нашлось ${islands} остров(а)`);
   return islands;
 }
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+  let count = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === '1') {
+        count++;
+        dfs(i, j);
+      }
+    }
+  }
+
+  function dfs(i, j) {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === '0') {
+      return;
+    }
+
+    grid[i][j] = '0';
+
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i, j - 1);
+  }
+
+  return count;
+};
