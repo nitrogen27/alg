b1f3418142f2fa1cc972e4953e1b2a19972fcba1
nitrogen27 <nitrogen2772@gmail.com>
2025-03-24 16:22:21 +0300

    Update intervals.js

--- sort/intervals.js
+++ sort/intervals.js
@@ -1,1 +1,20 @@
-
+var merge = function (intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    let merged = [];
+    for (let interval of intervals) {
+        // if the list of merged intervals is empty or if the current
+        // interval does not overlap with the previous, simply append it.
+        if (merged.length === 0 || merged[merged.length - 1][1] < interval[0]) {
+            merged.push(interval);
+        }
+        // otherwise, there is overlap, so we merge the current and previous
+        // intervals.
+        else {
+            merged[merged.length - 1][1] = Math.max(
+                merged[merged.length - 1][1],
+                interval[1],
+            );
+        }
+    }
+    return merged;
+};
