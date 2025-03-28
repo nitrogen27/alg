6518970b956dcfec1a7ba0efc07f7384f1705190
nitrogen27 <nitrogen2772@gmail.com>
2025-03-28 13:31:09 +0300

    Update frequent_element.js

--- heap/frequent_element.js
+++ heap/frequent_element.js
@@ -1,1 +1,22 @@
+export function Frequent_elements(nums, k) {
+  const hash = new Map();
+  const heap = [];
+  const result = [];
+
+  for (let n of nums) {
+    hash.set(n, hash.has(n) ? hash.get(n) + 1 : 1);
+  }
+
+  for (let [key, value] of Array.from(hash)) {
+    heap[value] = key;
+  }
+  while (heap.length) {
+    if (result.length < k) {
+      result.push(heap.pop());
+    } else {
+      console.log(result);
+      return result;
+    }
+  }
+}
 
