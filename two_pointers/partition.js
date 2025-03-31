85fa97082b02c38a1af60feb8b691a02254dc145
nitrogen27 <nitrogen2772@gmail.com>
2025-03-31 11:11:03 +0300

    Update partition.js

--- two_pointers/partition.js
+++ two_pointers/partition.js
@@ -1,1 +1,12 @@
+Example 1:
 
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+Example 2:
+
+Input: s = "eccbbbbdec"
+Output: [10]
