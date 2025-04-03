9e4761739e6bb2ed967fd94dba89cb3612ec32ef
nitrogen27 <nitrogen2772@gmail.com>
2025-04-03 14:09:19 +0300

    Update max.js

--- silding_window/max.js
+++ silding_window/max.js
@@ -36,25 +36,6 @@ export function max(nums, k) {
       help.pop();
     }
     help.push(i);
-
-    if (i - start + 1 === k) {
-      result.push(nums[help[0]]);
-      start++;
-    }
-  }
-  console.log(result);
-  return result;
-}
-export function max(nums, k) {
-  const help = [];
-  const result = [];
-  let start = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    while (nums[i] > nums[help[help.length - 1]]) {
-      help.pop();
-    }
-    help.push(i);
     if (i - start + 1 === k) {
       result.push(nums[help[0]]);
       if(start === help[0]){
