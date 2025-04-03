a9814495c9cb80da32cc390fc4c8467ce862b0c5
nitrogen27 <nitrogen2772@gmail.com>
2025-04-03 17:25:49 +0300

    Update replacement.js

--- silding_window/replacement.js
+++ silding_window/replacement.js
@@ -10,3 +10,38 @@ Output: 4
 Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
 The substring "BBBB" has the longest repeating letters, which is 4.
 There may exists other ways to achieve this answer too.
+export function replacement(s, k) {
+  let start = 0;
+  let end = 0;
+  let result = 0;
+  const fr = new Array(26).fill(0);
+  const help = [];
+
+  while (end < s.length) {
+    fr[s.charCodeAt(end) - "A".charCodeAt(0)]++;
+    while (
+      fr[s.charCodeAt(end) - "A".charCodeAt(0)] >
+      fr[s.charCodeAt(help[help.length - 1]) - "A".charCodeAt(0)]
+    ) {
+      help.pop();
+    }
+    help.push(end);
+    let need = end - start + 1 -  fr[s.charCodeAt(help[help.length - 1]) - "A".charCodeAt(0)];
+    if (need > k) {
+      if (
+        fr[s.charCodeAt(start) - "A".charCodeAt(0)] ===
+        fr[s.charCodeAt(help[0]) - "A".charCodeAt(0)]
+      ) {
+        help.shift();
+      }
+      start++;
+    }
+    if (need < k) {
+      if (end - start + 1 > result) {
+        result = end - start + 1;
+      }
+      end++;
+    }
+  }
+  return result;
+}
