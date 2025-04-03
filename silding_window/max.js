ef2dfb9dbb63d6da51777e3328ca21ae4472ab4b
nitrogen27 <nitrogen2772@gmail.com>
2025-04-03 13:34:48 +0300

    Update max.js

--- silding_window/max.js
+++ silding_window/max.js
@@ -16,3 +16,22 @@ Example 2:
 
 Input: nums = [1], k = 1
 Output: [1]
+export function max(nums, k) {
+  const help = [];
+  const result = [];
+  let start = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    while (nums[i] > nums[help[help.length - 1]]) {
+      help.pop();
+    }
+    help.push(i);
+
+    if (i - start + 1 === k) {
+      result.push(nums[help[0]]);
+      start++;
+    }
+  }
+  console.log(result);
+  return result;
+}
