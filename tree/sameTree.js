c21fc511ebc744497018a02dc9f68e54eeb20a27
nitrogen27 <nitrogen2772@gmail.com>
2025-04-09 14:35:12 +0300

    Update sameTree.js

--- tree/sameTree.js
+++ tree/sameTree.js
@@ -46,3 +46,42 @@ export function sameTree(p, q) {
   console.log(isSame(p, q));
   return isSame(p, q);
 }
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    function isSame(p, q) {
+        if(!p && !q){
+            return true
+        }
+    if (p?.val !== q?.val) {
+      return false;
+    }
+    let left , right = false;
+    if(p.left !== null && q.left !== null){
+     left = isSame(p.left, q.left);
+    }
+    if(p.right !== null && q.right !== null){
+      right =  isSame(p.right, q.right);
+    }
+    if (p.left === null && q.left === null) {
+      left = true;
+    }
+    if (p.right === null && q.right === null) {
+      right = true;
+    }
+    return !(left === false || right === false);
+
+  }
+  return isSame(p, q);
+};
