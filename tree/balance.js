14b1cab2769e0110cbaaf6b502d6c5ed0e040fc8
nitrogen27 <nitrogen2772@gmail.com>
2025-04-13 17:09:26 +0300

    Update balance.js

--- tree/balance.js
+++ tree/balance.js
@@ -24,3 +24,29 @@ function isBalanced(root) {
     
     return checkHeight(root) !== -1;
 }
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    function getHeight(node){
+        if(!node){
+            return 0
+        }
+       return 1 + (Math.max(getHeight(node.right),getHeight(node.left))); 
+    }
+
+    if(!root){
+        return true
+    }
+
+    return Math.abs(getHeight(root.right)- getHeight(root.left)) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+};
