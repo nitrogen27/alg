3e776bc408f05d22f9321a3c1d37a81779ce4c03
nitrogen27 <nitrogen2772@gmail.com>
2025-04-16 16:36:15 +0300

    Create zero-count.js

---
+++ silding_window/zero-count.js
@@ -0,0 +1,26 @@
+export function counter(nums) {
+  let left = 0;
+  let right = 0;
+  let count = 0;
+  let result = 0;
+
+  while (right < nums.length) {
+    if(nums[right] === 0 && count < 2){
+      count++
+    }
+    if(count < 2){
+      result = Math.max(result, right - left + 1 - count)
+      right++
+    }
+    if(count >= 2){
+      if(nums[left] === 0){
+        count--;
+        right++;
+      }
+      left++
+    }
+
+  }
+  console.log("result", result)
+  return result;
+}
