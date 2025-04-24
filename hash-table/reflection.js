76dc9c056da5e5f0e0b36d1366e54613d08f55f6
nitrogen27 <nitrogen2772@gmail.com>
2025-04-24 13:18:42 +0300

    Update reflection.js

--- hash-table/reflection.js
+++ hash-table/reflection.js
@@ -1,19 +1,8 @@
-/*
-
-1       |   5  7
- 2      |  4
-   5    |      7
-
-  axis = (maxX - minX)/2
-  maxX > axis maxX = axis * 2 + minX
-  maxX < axis minX = maxX - axis * 2
-
- */
-
 export function isReflection(points) {
   const hash = new Map();
   let minX = Infinity,
     maxX = -Infinity;
+  let pMin = 1;
   for (let point of points) {
     const [x, y] = point;
     minX = Math.min(minX, x);
@@ -24,18 +13,18 @@ export function isReflection(points) {
       hash.set(x, [y]);
     }
   }
-  let axis =  (maxX - minX)/2
-  if(minX < 0 && maxX > 0 && maxX <= Math.abs(minX)){
-    axis = -(maxX - Math.abs(minX))/2
+  let axis = (maxX - minX) / 2;
+  if (minX < 0 && maxX > 0 && maxX <= Math.abs(minX)) {
+    axis = -(maxX - Math.abs(minX)) / 2;
+    pMin = -1;
   }
-
   for (let point of points) {
     const [x, y] = point;
     let pairX;
     if (x === axis) {
       pairX = axis;
     } else {
-      pairX = x > axis ? x - 2 * axis : 2 * axis + x;
+      pairX = x > axis ? (x - 2 * axis) * pMin : 2 * axis + x;
     }
     if (!hash.has(pairX)) {
       return false;
