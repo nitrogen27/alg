46568c98f8fc8a49e886157933b93a0643b006da
nitrogen27 <nitrogen2772@gmail.com>
2025-04-28 15:15:11 +0300

    Update compress.js

--- two_pointers/compress.js
+++ two_pointers/compress.js
@@ -95,3 +95,30 @@ export function compress_in_place(chars) {
   console.log(chars.slice(0, left));
   return chars.slice(0, left);
 }
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    let n = chars.length;
+    let idx = 0;
+    for (let i = 0; i < n; i++) {
+        let ch = chars[i];
+        let count = 0;
+        while (i < n && chars[i] === ch) {
+            count++;
+            i++;
+        }
+        if (count === 1) {
+            chars[idx++] = ch;
+        } else {
+            chars[idx++] = ch;
+            for (let digit of count.toString()) {
+                chars[idx++] = digit;
+            }
+        }
+        i--;
+    }
+    chars.length = idx;
+    return idx;
+};
