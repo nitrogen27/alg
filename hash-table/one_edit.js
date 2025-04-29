81ac56ff61aa06caa2c0c494d7c5cec22e2177a0
nitrogen27 <nitrogen2772@gmail.com>
2025-04-29 17:36:02 +0300

    Update one_edit.js

--- hash-table/one_edit.js
+++ hash-table/one_edit.js
@@ -67,3 +67,92 @@ export function one_edit(s, t) {
   }
   return sCount - t.length === 0 && tCount === 1;
 }
+/*
+"ABC" "ABMC" true  add
+
+"ABC" "ABBC" true  add
+
+"ABC"  "AB" true   delete
+
+"ABC"  "ABM" true  change
+
+"ABC" "ABMC
+
+left right
+
+1. A === A left = 0 right = 0
+2. B === B left = 1 right = 1
+3. C !== M left = 2 right = 2 count = 1
+
+3. C !== M left = 2 right = 2 count = 1
+
+
+
+"ABMC" "ABC"
+
+1. A === A left = 0 right = 0
+2. B === B left = 1 right = 1
+3. M !== C left = 2 right = 2 count = 1
+4. C === C left = 3 right = 2
+
+"ABC"  "ABM"
+
+1. A === A left = 0 right = 0
+2. B === B left = 1 right = 1
+3. M !== C left = 3 right = 3 count = 1
+
+"ABCD" "DCB"
+1. A !== D left = 0 right = 0 count = 1
+2. B !== C left = 1 right = 1 count = 2
+
+"MABC"   "ABC"
+
+1. M !== A left = 0 right = 0 count = 1
+2. A === A left = 1 right = 0
+3. B === B left = 2 right = 1
+4. C === B left = 3 right = 2
+
+
+"AB"   "AB"
+
+1. A === A left = 0 right = 0 count = 1
+2. A === A left = 1 right = 0
+3. B === B left = 2 right = 1
+4. C === B left = 3 right = 2
+
+"AB"   "ABC"
+
+1. A === A left = 0 right = 0
+2. B === B left = 1 right = 1
+
+"ABC"   "ABM"
+
+1. A === A left = 0 right = 0
+2. B === B left = 1 right = 1
+2. C !== M left = 2 right = 2 count = 1
+
+right не двигаем если нет совпадения
+ */
+
+export function one_edit(s, t) {
+  let left = 0;
+  let right = 0;
+  let count = 0;
+
+  while (left < s.length) {
+    if (s[left] !== t[right]) {
+      count++;
+    } else {
+      right++;
+    }
+    left++;
+    if (count > 1) {
+      return false;
+    }
+  }
+  if (t.length > s.length && t[right]) {
+   count++;
+  }
+  console.log(left, right, count)
+  return count === 1;
+}
