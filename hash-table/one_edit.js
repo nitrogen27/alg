ff706062212274a9e63d6ea39b2e2bd7428d15f0
nitrogen27 <nitrogen2772@gmail.com>
2025-04-30 14:53:12 +0300

    Update one_edit.js

--- hash-table/one_edit.js
+++ hash-table/one_edit.js
@@ -106,3 +106,27 @@ export function one_edit(s, t) {
   }
   return count === 1;
 }
+var isOneEditDistance = function (s, t) {
+    let ns = s.length;
+    let nt = t.length;
+
+    // Ensure that s is shorter than t.
+    if (ns > nt) return isOneEditDistance(t, s);
+
+    // The strings are NOT one edit away distance
+    // if the length diff is more than 1.
+    if (nt - ns > 1) return false;
+
+    for (let i = 0; i < ns; i++)
+        if (s[i] != t[i])
+            if (ns == nt)
+                // if strings have the same length
+                return s.slice(i + 1) === t.slice(i + 1);
+            // If strings have different lengths
+            else return s.slice(i) === t.slice(i + 1);
+
+    // If there are no diffs in ns distance
+    // The strings are one edit away only if
+    // t has one more character.
+    return ns + 1 === nt;
+};
