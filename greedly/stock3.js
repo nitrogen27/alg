671be0ec1b656e3b97ee0877fa7abddc530f4fc1
nitrogen27 <nitrogen2772@gmail.com>
2025-05-30 16:13:54 +0300

    Update stock3.js

--- greedly/stock3.js
+++ greedly/stock3.js
@@ -71,3 +71,11 @@ export function stock3(prices, fee) {
   console.log(0);
   return 0;
 }
+var maxProfit = function(P, F) {
+    let len = P.length, buying = 0, selling = -P[0]
+    for (let i = 1; i < len; i++) {
+        buying = Math.max(buying, selling + P[i] - F)
+        selling = Math.max(selling, buying - P[i])
+    }
+    return buying
+};
